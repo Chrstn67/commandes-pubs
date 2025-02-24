49ce7c2ef9e3de45d2df2de2a68566d5e08e5e97
Chrstn67 <chrstn.hmbrt67.com>
2025-02-24 13:36:56 +0100

    MAJ

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+import Commandes from "../Commandes/Commandes";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>Commandes de publications</header>
+      <Commandes />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Commandes/data.jsx
@@ -0,0 +1,786 @@
+const data = [
+  {
+    nom: "ÉTOUKÉ Franck",
+    groupe: "1",
+    commandes: [],
+  },
+  {
+    nom: "GOTTI Xavier",
+    groupe: "1",
+    commandes: [],
+  },
+  {
+    nom: "BINDER Raymond",
+    groupe: "1",
+    commandes: [],
+  },
+  {
+    nom: "BODEMER Emmanuel",
+    groupe: "1",
+    commandes: [],
+  },
+  {
+    nom: "COUDRET Roland",
+    groupe: "3",
+    commandes: [
+      {
+        type: "Permanente",
+        publication: "TG Étude",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+      {
+        type: "Permanente",
+        publication: "Cahier Vie",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+    ],
+  },
+  {
+    nom: "FUSS Kévin",
+    groupe: "1",
+    commandes: [],
+  },
+
+  {
+    nom: "MNEMOSYME Éva",
+    groupe: "1",
+    commandes: [],
+  },
+
+  {
+    nom: "REYNAUD Lydie",
+    groupe: "1",
+    commandes: [],
+  },
+  {
+    nom: "WERNERT Jean-Marc",
+    groupe: "2",
+    commandes: [],
+  },
+  {
+    nom: "LEDRICH Pascal",
+    groupe: "1",
+    commandes: [],
+  },
+  {
+    nom: "BODEMER Alfred",
+    groupe: "2",
+    commandes: [],
+  },
+  {
+    nom: "GRISLIN Véronique",
+    groupe: "2",
+    commandes: [],
+  },
+  {
+    nom: "LANDMANN Cathy",
+    groupe: "2",
+    commandes: [],
+  },
+  {
+    nom: "LEY Claude",
+    groupe: "2",
+    commandes: [
+      {
+        type: "Permanente",
+        publication: "TG Étude",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+      {
+        type: "Permanente",
+        publication: "Cahier Vie",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+    ],
+  },
+  {
+    nom: "MARAINE Daniel",
+    groupe: "2",
+    commandes: [
+      {
+        type: "Permanente",
+        publication: "TG Étude",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+      {
+        type: "Permanente",
+        publication: "Cahier Vie",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+    ],
+  },
+  {
+    nom: "MARCHAL Sonia",
+    groupe: "2",
+    commandes: [
+      {
+        type: "Permanente",
+        publication: "TG Étude",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+
+      {
+        type: "Permanente",
+        publication: "Revues publiques",
+        details: [{ langue: "Allemand", quantite: "1" }],
+      },
+    ],
+  },
+  {
+    nom: "PELLÉ Joseph",
+    groupe: "2",
+    commandes: [
+      {
+        type: "Permanente",
+        publication: "TG Étude",
+        details: [{ langue: "Français", quantite: "2" }],
+      },
+      {
+        type: "Permanente",
+        publication: "Cahier Vie",
+        details: [{ langue: "Français", quantite: "2" }],
+      },
+    ],
+  },
+  {
+    nom: "PIEDOIE Patricia",
+    groupe: "2",
+    commandes: [],
+  },
+  {
+    nom: "STEPHAN Geneviève",
+    groupe: "2",
+    commandes: [],
+  },
+  {
+    nom: "TROUDART Reno",
+    groupe: "2",
+    commandes: [
+      {
+        type: "Permanente",
+        publication: "TG Étude",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+      {
+        type: "Permanente",
+        publication: "Cahier Vie",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+    ],
+  },
+  {
+    nom: "WITTERSHEIM Anne-Laure",
+    groupe: "2",
+    commandes: [],
+  },
+  {
+    nom: "BECUWE Sébastien",
+    groupe: "3",
+    commandes: [],
+  },
+
+  {
+    nom: "HALTER Valentin",
+    groupe: "6",
+    commandes: [],
+  },
+  {
+    nom: "HUMBERT Christian",
+    groupe: "3",
+    commandes: [],
+  },
+  {
+    nom: "MAETZ Marie-Thérèse",
+    groupe: "3",
+    commandes: [],
+  },
+  {
+    nom: "SELVA Agnès",
+    groupe: "3",
+    commandes: [
+      {
+        type: "Permanente",
+        publication: "TG Étude",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+      {
+        type: "Permanente",
+        publication: "Cahier Vie",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+
+      {
+        type: "Annuelle",
+        publication: "Examinons les Écritures",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+      {
+        type: "Annuelle",
+        publication: "Volumes Reliés",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+    ],
+  },
+
+  {
+    nom: "GRECO Pascal",
+    groupe: "5",
+    commandes: [
+      {
+        type: "Permanente",
+        publication: "TG Étude",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+      {
+        type: "Permanente",
+        publication: "Cahier Vie",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+      {
+        type: "Permanente",
+        publication: "Revues publiques",
+        details: [{ langue: "Allemand", quantite: "1" }],
+      },
+    ],
+  },
+  {
+    nom: "BEGERT Paulette",
+    groupe: "3",
+    commandes: [
+      {
+        type: "Permanente",
+        publication: "TG Étude",
+        details: [{ langue: "Français", quantite: "2" }],
+      },
+      {
+        type: "Permanente",
+        publication: "Cahier Vie",
+        details: [{ langue: "Français", quantite: "2" }],
+      },
+      {
+        type: "Annuelle",
+        publication: "Examinons les Écritures",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+      {
+        type: "Annuelle",
+        publication: "Volumes Reliés",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+    ],
+  },
+  {
+    nom: "DI MAGGIO Lorenzo",
+    groupe: "1",
+    commandes: [
+      {
+        type: "Permanente",
+        publication: "TG Étude",
+        details: [{ langue: "Français", quantite: "2" }],
+      },
+      {
+        type: "Permanente",
+        publication: "Cahier Vie",
+        details: [{ langue: "Français", quantite: "2" }],
+      },
+      {
+        type: "Permanente",
+        publication: "Revues publiques",
+        details: [{ langue: "Allemand", quantite: "1" }],
+      },
+    ],
+  },
+  {
+    nom: "HIMBER Anne",
+    groupe: "7",
+    commandes: [],
+  },
+  {
+    nom: "KOESTERS Jean-Pierre",
+    groupe: "2",
+    commandes: [
+      {
+        type: "Permanente",
+        publication: "TG Étude",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+      {
+        type: "Permanente",
+        publication: "TG Étude GC",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+      {
+        type: "Permanente",
+        publication: "Cahier Vie",
+        details: [{ langue: "Français", quantite: "2" }],
+      },
+      {
+        type: "Annuelle",
+        publication: "Volumes Reliés",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+      {
+        type: "Annuelle",
+        publication: "Examinons les Écritures",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+      {
+        type: "Annuelle",
+        publication: "Examinons les Écritures GC",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+    ],
+  },
+  {
+    nom: "KOESTERS Muriel",
+    groupe: "2",
+    commandes: [
+      {
+        type: "Permanente",
+        publication: "TG Étude",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+      {
+        type: "Permanente",
+        publication: "Cahier Vie",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+      {
+        type: "Annuelle",
+        publication: "Examinons les Écritures",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+      {
+        type: "Annuelle",
+        publication: "Volumes Reliés",
+        details: [{ langue: "Français", quantite: "2" }],
+      },
+    ],
+  },
+  {
+    nom: "LAVOUIRAY Patrick",
+    groupe: "5",
+    commandes: [],
+  },
+  {
+    nom: "SAKAC Nicolas",
+    groupe: "4",
+    commandes: [],
+  },
+  {
+    nom: "SCHLEISS Romain",
+    groupe: "6",
+    commandes: [],
+  },
+  {
+    nom: "MARRAZZO David",
+    groupe: "4",
+    commandes: [],
+  },
+  {
+    nom: "KALCK Francis",
+    groupe: "4",
+    commandes: [],
+  },
+  {
+    nom: "BODEMER Gérard",
+    groupe: "4",
+    commandes: [
+      {
+        type: "Permanente",
+        publication: "TG Étude",
+        details: [{ langue: "Français", quantite: "2" }],
+      },
+      {
+        type: "Permanente",
+        publication: "Cahier Vie",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+      {
+        type: "Permanente",
+        publication: "Revues publiques",
+        details: [{ langue: "Allemand", quantite: "1" }],
+      },
+    ],
+  },
+  {
+    nom: "BOULET Véronique",
+    groupe: "4",
+    commandes: [],
+  },
+  {
+    nom: "MARRAZZO Vincent",
+    groupe: "5",
+    commandes: [],
+  },
+
+  {
+    nom: "MERIOUA Chaabane",
+    groupe: "4",
+    commandes: [],
+  },
+  {
+    nom: "WERNERT Serge",
+    groupe: "3",
+    commandes: [
+      {
+        type: "Annuelle",
+        publication: "Examinons les Écritures",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+    ],
+  },
+  {
+    nom: "HUTTENBERGER Philippe",
+    groupe: "5",
+    commandes: [
+      {
+        type: "Permanente",
+        publication: "TG Étude",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+    ],
+  },
+  {
+    nom: "SELVA Frédéric",
+    groupe: "5",
+    commandes: [],
+  },
+  {
+    nom: "BATARD Roseline",
+    groupe: "5",
+    commandes: [],
+  },
+  {
+    nom: "CHAMPREDONDE Jean-Marc",
+    groupe: "5",
+    commandes: [],
+  },
+  {
+    nom: "DUDICOURT Michel",
+    groupe: "5",
+    commandes: [],
+  },
+  {
+    nom: "FRADE José",
+    groupe: "4",
+    commandes: [],
+  },
+  {
+    nom: "MEYER Rémy",
+    groupe: "5",
+    commandes: [],
+  },
+  {
+    nom: "RABOTEUR Natacha",
+    groupe: "5",
+    commandes: [
+      {
+        type: "Permanente",
+        publication: "TG Étude",
+        details: [{ langue: "Français", quantite: "2" }],
+      },
+      {
+        type: "Permanente",
+        publication: "Cahier Vie",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+    ],
+  },
+  {
+    nom: "SCHLEISS Amandine",
+    groupe: "5",
+    commandes: [],
+  },
+  {
+    nom: "SCHLEISS Sylvain",
+    groupe: "5",
+    commandes: [],
+  },
+  {
+    nom: "POULET Patrick",
+    groupe: "6",
+    commandes: [],
+  },
+  {
+    nom: "WERNERT Vincent",
+    groupe: "6",
+    commandes: [],
+  },
+  {
+    nom: "CLAR Raymond",
+    groupe: "6",
+    commandes: [
+      {
+        type: "Permanente",
+        publication: "TG Étude",
+        details: [{ langue: "Français", quantite: "3" }],
+      },
+    ],
+  },
+  {
+    nom: "FRISCH Mickaël",
+    groupe: "6",
+    commandes: [],
+  },
+  {
+    nom: "FURST Christiane",
+    groupe: "6",
+    commandes: [
+      {
+        type: "Permanente",
+        publication: "TG Étude",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+      {
+        type: "Permanente",
+        publication: "Cahier Vie",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+    ],
+  },
+  {
+    nom: "GRUNY Michaël",
+    groupe: "6",
+    commandes: [
+      {
+        type: "Permanente",
+        publication: "TG Étude",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+      {
+        type: "Permanente",
+        publication: "Cahier Vie",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+    ],
+  },
+  {
+    nom: "MITHRIDATE Jean-Fred",
+    groupe: "6",
+    commandes: [],
+  },
+  {
+    nom: "NAAS Alphonse",
+    groupe: "6",
+    commandes: [
+      {
+        type: "Permanente",
+        publication: "TG Étude",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+      {
+        type: "Permanente",
+        publication: "TG Étude GC",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+      {
+        type: "Permanente",
+        publication: "Cahier Vie",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+    ],
+  },
+  {
+    nom: "PACIOTTI Lydia",
+    groupe: "6",
+    commandes: [
+      {
+        type: "Permanente",
+        publication: "TG Étude",
+        details: [{ langue: "Français", quantite: "3" }],
+      },
+      {
+        type: "Permanente",
+        publication: "Cahier Vie",
+        details: [{ langue: "Français", quantite: "3" }],
+      },
+      {
+        type: "Annuelle",
+        publication: "Examinons les Écritures",
+        details: [{ langue: "Français", quantite: "2" }],
+      },
+    ],
+  },
+  {
+    nom: "POULET Marion",
+    groupe: "6",
+    commandes: [],
+  },
+  {
+    nom: "SCHLEISS Marcel",
+    groupe: "6",
+    commandes: [
+      {
+        type: "Permanente",
+        publication: "TG Étude",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+      {
+        type: "Permanente",
+        publication: "Cahier Vie",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+    ],
+  },
+
+  {
+    nom: "TAHAR Victor",
+    groupe: "7",
+    commandes: [],
+  },
+  {
+    nom: "BRUCKER Hervé",
+    groupe: "7",
+    commandes: [],
+  },
+  {
+    nom: "DAVID Estelle",
+    groupe: "7",
+    commandes: [],
+  },
+  {
+    nom: "HENRICH Béatrice",
+    groupe: "7",
+    commandes: [
+      // {
+      //   type: "Permanente",
+      //   publication: "TG Étude",
+      //   details: [{ langue: "Français", quantite: "1" }],
+      // },
+      // {
+      //   type: "Permanente",
+      //   publication: "Cahier Vie",
+      //   details: [{ langue: "Français", quantite: "1" }],
+      // },
+      // {
+      //   type: "Permanente",
+      //   publication: "Revues publiques",
+      //   details: [{ langue: "Allemand", quantite: "1" }],
+      // },
+    ],
+  },
+  {
+    nom: "MULLER Georgette",
+    groupe: "7",
+    commandes: [],
+  },
+  {
+    nom: "MULLER Selvy",
+    groupe: "7",
+    commandes: [],
+  },
+  {
+    nom: "PRABHAKAR Patricia",
+    groupe: "7",
+    commandes: [],
+  },
+  {
+    nom: "LE PIEZ Patrick",
+    groupe: "3",
+    commandes: [
+      {
+        type: "Permanente",
+        publication: "TG Étude",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+    ],
+  },
+  {
+    nom: "RAETH Gérard",
+    groupe: "7",
+    commandes: [],
+  },
+  {
+    nom: "BACH Jean-Claude",
+    groupe: "7",
+    commandes: [],
+  },
+
+  {
+    nom: "BINDER Pierre",
+    groupe: "?",
+    commandes: [],
+  },
+  {
+    nom: "BUCHMANN Bernard",
+    groupe: "7",
+    commandes: [],
+  },
+  {
+    nom: "ERNWEIN Pierre",
+    groupe: "3",
+    commandes: [
+      {
+        type: "Permanente",
+        publication: "TG Étude",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+      {
+        type: "Permanente",
+        publication: "Cahier Vie",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+    ],
+  },
+  {
+    nom: "KRAEMER Agnès",
+    groupe: "5",
+    commandes: [],
+  },
+  {
+    nom: "NUSSBAUM Patricia",
+    groupe: "7",
+    commandes: [
+      {
+        type: "Permanente",
+        publication: "TG Étude",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+      {
+        type: "Permanente",
+        publication: "Cahier Vie",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+    ],
+  },
+  {
+    nom: "STEINMETZ André",
+    groupe: "4",
+    commandes: [],
+  },
+  {
+    nom: "RIETSCH Jean-Michel",
+    groupe: "7",
+    commandes: [],
+  },
+  {
+    nom: "BARRET Hubert",
+    groupe: "2",
+    commandes: [
+      {
+        type: "Permanente",
+        publication: "TG Étude",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+
+      {
+        type: "Permanente",
+        publication: "Cahier Vie",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+      {
+        type: "Annuelle",
+        publication: "Volumes Reliés",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+      {
+        type: "Annuelle",
+        publication: "Examinons les Écritures GC",
+        details: [{ langue: "Français", quantite: "1" }],
+      },
+    ],
+  },
+];
+
+export default data;
